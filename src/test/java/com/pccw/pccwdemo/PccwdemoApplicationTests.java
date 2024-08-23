1761c73342ddd8adfcd4578a7553905e0c49d1e0
DanG1968 <dangrin1968@gmail.com>
2024-08-23 17:25:06 +0800

    Initial commit

---
+++ src/test/java/com/pccw/pccwdemo/PccwdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pccw.pccwdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PccwdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
